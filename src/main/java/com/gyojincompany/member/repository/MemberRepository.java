70799959c85f6bab3eaa000fdffe2508940767ca
gyojincompany <luckyred@gmail.com>
2023-07-10 13:03:45 +0900

    update 및 다양한 select 조건 추가

--- src/main/java/com/gyojincompany/member/repository/MemberRepository.java
+++ src/main/java/com/gyojincompany/member/repository/MemberRepository.java
@@ -3,6 +3,7 @@ package com.gyojincompany.member.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.gyojincompany.member.entity.Member;
 
@@ -26,5 +27,13 @@ public interface MemberRepository extends JpaRepository<Member, Long>{
 	public List<Member> findByMageGreaterThan(int mage);//몇살 초과만 검색
 	public List<Member> findByMageGreaterThanEqual(int mage);//몇살 이상만 검색
 	public List<Member> findByMageBetween(int mage1, int mage2);//mage1 <= x <= mage2
-
+	
+	public List<Member> findByMnameStartingWith(String firstName);//특정단어로 시작
+	public List<Member> findByMnameEndingWith(String lastName);//특정단어로 끝남
+	public List<Member> findByMnameContaining(String midName);//특정단어가 포함
+	public List<Member> findByMnameLike(String namekey);//특정단어가 포함
+	
+	@Transactional
+	public void deleteAllByMname(String mname);//이름으로 검색하여 삭제
+	
 }
--- src/test/java/com/gyojincompany/member/JpaTest.java
+++ src/test/java/com/gyojincompany/member/JpaTest.java
@@ -1,6 +1,9 @@
 package com.gyojincompany.member;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -57,6 +60,41 @@ public class JpaTest {
 		memberRepository.save(member);//insert into jpa_member value();
 		
 	}
+	
+	@Test
+	@DisplayName("특정 단어가 포함된 이름 검색")
+	public void searchKeyName() {
+		List<Member> memberlist = memberRepository.findByMnameLike("%길%");
+		for(Member member : memberlist) {
+			System.out.println(member.toString());
+		}
+	}
+	
+	
+	@Test
+	@DisplayName("특정 이름으로 검색하여 삭제")
+	public void deleteMember() {
+		memberRepository.deleteAllByMname("이순신");
+	}
+	
+	@Test
+	@DisplayName("회원 정보 수정")
+	public void modifyMember() {
+		
+		Optional<Member> memberOptional = memberRepository.findById(15L);//mnum(기본키)가 2번인 회원 조회
+		//Optional 타입으로 반환받으면 null 값으로 반환되었을때도 에러가 안나고 null값으로 저장
+		
+		assertTrue(memberOptional.isPresent());//null값 여부 확인(테스트용)
+		
+		Member member = memberOptional.get();
+		member.setMname("강감찬");//이름 수정
+		member.setMage(28);//나이 수정
+		
+		memberRepository.save(member);
+	}
+	
+	
+	
 	
 	
 	
